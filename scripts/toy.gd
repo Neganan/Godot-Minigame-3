ca7967d72c5a8f7d43889ab43f5f4d816f9211ba
plasticmuncher <157843535+Neganan@users.noreply.github.com>
2026-05-15 18:18:17 -0700

    LETS FUCKING RESTART NOW WOOOOOOOO

--- scripts/toy.gd
+++ scripts/toy.gd
@@ -54,11 +54,49 @@ func _add(a: int, b: int) -> int:
 func _playground() -> void:
 	# playground 1
 	var message = "Hello world!"
-	
-
+	print(message)
+	# playground 2
+	var seconds = 12.5
+	message = "seconds = %f" % seconds
+	print(message)  
+	# playground 3
+	message = "seconds = %.1f" % seconds
+	print(message)
+	# playground 4
+	var x = 1.0
+	var y = 3.4
+	var z = 20.5
+	message = "position = (%.1f, %.1f, %.1f)" % [x, y, z]
+	print(message)
+	var v1 = 4
+	var v2 = 3
+	var sum_1_2 = _add(v1, v2)
+	print("sum: %s" % sum_1_2)
+	sum_1_2 = _add(10, 2)
+	print("second sum: %s" % sum_1_2)
+	_print_name("Y/N")
+	var foo = true
+	print (foo)
 # playground 8
 # put ya func here
-
+func _print_name(n: String) -> void:
+	print("your name is: %s" % n)
+	var x = -1.0
+	var y = 19.1
+	var xLarger = _checkIfXIsLarger(x, y)
+	print("is x larger? %s" % xLarger)
+	x = 1
+	y = 1
+	var comparison = x !=y
+	print("comparison result: %s" % comparison)
+	x = 4
+	y = 3
+	var w = 10
+	var u = 0
+	comparison = x > y && w < u
+	print("comparison result 2: %s" % comparison)
 
 # playground 11
 # put ya func here
+func _checkIfXIsLarger(x: float, y: float) -> bool:
+		return x > y
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,9 +4,9 @@ extends HBoxContainer
 @onready var _colorText = get_node("ColorLabel")
 
 # minigame step 1
-# func _update_Bounces??:
-#	var text = ??
-#	_bounceText.text = text
+#func _update_Bounces(n: int) -> void:
+	#var text = "bounces: %s" % bounces
+	#_bounceText.text = text
 	
 
 # minigame step 5
